b250b32a791ba35a55631c72d97257e538ee0675
odhoman <jonatanflores2004@yahoo.com.ar>
2017-01-17 11:31:13 -0300

    Push Inicial del Servicio Rest

---
+++ rest_service/src/main/java/hello/Libro.java
@@ -0,0 +1,52 @@
+package hello;
+
+public class Libro {
+
+	private Long id;
+	private String autor;
+	private String titulo;
+	private String editorial;
+
+	public Libro(Long id, String autor, String titulo, String editorial) {
+
+		this.id = id;
+		this.autor = autor;
+		this.titulo = titulo;
+		this.editorial = editorial;
+	}
+
+	public Libro() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+}
